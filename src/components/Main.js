a8fd7e1fba9709a5f6f18cde2f22c13598fdadf0
Carolina Nonato <csnonato@gmail.com>
2022-05-01 22:49:28 -0400

    main price section

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,19 +6,19 @@ function Main() {
     return (
         <div className='main-container'>
             <img src={hero} className="hero-image" alt="" />
-            <h3>Order Summary</h3>
+            <p className="main-title">Order Summary</p>
             <p className='main-paragraph'>You can now listem to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
             <div className="main-price">
                 <img src={music} alt="" />
                 <div className="main-plan">
                     <p className="plan-type">Annual Plan</p>
-                    <p>$59.99/year</p>
+                    <p className='plan-price'>$59.99/year</p>
                 </div>
                 <a href="#">Change</a>
             </div>
             <button className="main-button">Proceed to Payment</button>
             <p>Cancel Order</p>
-        </div>
+        </div >
     )
 }
 
